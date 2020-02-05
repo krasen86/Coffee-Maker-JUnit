65b214313f325a71a8a441f22aa3eb0c3aec3e01
Leith Hobson <leith@hobson.co.za>
2020-02-05 18:07:25 +0100

    Commit MainTest class progress

--- unittests/edu/ncsu/csc326/coffeemaker/MainTest.java
+++ unittests/edu/ncsu/csc326/coffeemaker/MainTest.java
@@ -1,7 +1,68 @@
 package edu.ncsu.csc326.coffeemaker;
 
-import edu.ncsu.csc326.coffeemaker.exceptions.InventoryException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
 
 public class MainTest{
 
+    Assertions a;
+    Main main;
+
+    @BeforeEach
+    void setUp() {
+        main = new Main();
+    }
+
+    @AfterEach
+    void tearDown() {
+
+    }
+
+    @Test
+    void testExitMethodMainMenu() throws IOException {
+//        setInputStream("1\nA Recipe\n23\n2\n2\n1\n0\n0");
+//        main.mainMenu();
+    }
+
+    @Test
+    void addRecipe() {
+    }
+
+    @Test
+    void deleteRecipe() {
+    }
+
+    @Test
+    void editRecipe() {
+    }
+
+    @Test
+    void addInventory() {
+    }
+
+    @Test
+    void checkInventory() {
+    }
+
+    @Test
+    void makeCoffee() {
+    }
+
+    @Test
+    void main() {
+    }
+
+    void setInputStream(String input) throws IOException {
+        InputStream inputStream;
+        inputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStream);
+
+
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        System.out.println(br.readLine());
+    }
 }

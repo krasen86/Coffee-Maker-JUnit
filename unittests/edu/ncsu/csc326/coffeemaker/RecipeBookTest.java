e118b335823fd6de88b3d4f952d991c8f4c04e86
krasen <krasen@student.chalmers.se>
2020-02-05 15:58:31 +0100

    Added unit test for RecipeTest

--- unittests/edu/ncsu/csc326/coffeemaker/RecipeBookTest.java
+++ unittests/edu/ncsu/csc326/coffeemaker/RecipeBookTest.java
@@ -1,30 +1,84 @@
 package edu.ncsu.csc326.coffeemaker;
 import edu.ncsu.csc326.coffeemaker.exceptions.RecipeException;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.*;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 
 public class RecipeBookTest {
 
     private RecipeBook recipeBook;
-    private Recipe r1;
+    private Recipe recipeTest1;
+    private Recipe recipeTest2;
 
     @BeforeEach
-    public void setUp() throws RecipeException {
+    void setUp() throws RecipeException {
         recipeBook = new RecipeBook();
-        r1 = new Recipe();
-        r1.setName("Coffee");
-        r1.setAmtChocolate("0");
-        r1.setAmtCoffee("3");
-        r1.setAmtMilk("1");
-        r1.setAmtSugar("1");
-        r1.setPrice("50");
+        recipeTest1 = new Recipe();
+        recipeTest1.setName("Coffee");
+        recipeTest1.setAmtChocolate("0");
+        recipeTest1.setAmtCoffee("3");
+        recipeTest1.setAmtMilk("1");
+        recipeTest1.setAmtSugar("1");
+        recipeTest1.setPrice("50");
+
+        recipeTest2 = new Recipe();
+        recipeTest2.setName("Tea");
+        recipeTest2.setAmtChocolate("0");
+        recipeTest2.setAmtCoffee("3");
+        recipeTest2.setAmtMilk("1");
+        recipeTest2.setAmtSugar("1");
+        recipeTest2.setPrice("50");
     }
 
     @Test
-    public void testAddRecipe(){
-        Assertions.assertTrue(recipeBook.addRecipe(r1));
+    void testAddRecipe(){
+        Recipe[] recipes = recipeBook.getRecipes();
+        Assertions.assertTrue(recipeBook.addRecipe(recipeTest1));
+        Recipe recipeTest = recipes[0];
+        Assertions.assertEquals(recipeTest1, recipeTest);
+        Assertions.assertFalse(recipeBook.addRecipe(recipeTest1));
+    }
 
+    @Test
+    void testAddRecipeAddingTheSameRecipe(){
+        Recipe[] recipes = recipeBook.getRecipes();
+        recipeBook.addRecipe(recipeTest1);
+        Assertions.assertFalse(recipeBook.addRecipe(recipeTest1));
+    }
+
+    @Test
+    void testGetRecipeBooks(){
+        Recipe [] recipes = new Recipe[4];
+        Assertions.assertArrayEquals( recipes, recipeBook.getRecipes() );
+    }
+
+    @Test
+    void testDeleteRecipe(){
+        recipeBook.addRecipe(recipeTest1);
+        recipeBook.addRecipe(recipeTest2);
+        Recipe[] recipes = recipeBook.getRecipes();
+        recipeBook.deleteRecipe(0);
+        Recipe recipeTest = recipes[0];
+        Assertions.assertNotSame(recipeTest1, recipeTest);
+    }
+
+    @Test
+    void testDeleteRecipeThatIsNull(){
+        Assertions.assertNull(recipeBook.deleteRecipe(3));
+    }
+
+    @Test
+    void testEditRecipe(){
+        recipeBook.addRecipe(recipeTest1);
+        recipeBook.editRecipe(0, recipeTest2);
+        recipeTest2.setName("Coffee");
+        Recipe[] recipes = recipeBook.getRecipes();
+        Assertions.assertEquals(recipes[0], recipeTest2);
+    }
+
+    @Test
+    void testEditRecipeAtNullPosition() {
+        Assertions.assertNull(recipeBook.editRecipe(0, recipeTest2));
     }
 }
